e25d55dc3b1d41c85c69945b98444e5c40587e93
Int_21h <yuskovss@gmail.com>
2019-10-18 08:56:48 +0400

    feat: configured webpack and created minimal structure of the project

---
+++ readme.md
@@ -0,0 +1,6 @@
+Install Node.js
+Clone this repository https://github.com/dsyuskov/portfolio.git
+Go to folder repair-design-project
+Run npm install in command line
+Run npm run build
+Open index.html file in browser
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader', 
+        options: {
+            name: 'images/[name].[ext]'                    
+        }               
+    }, 
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./fonts/[name].[ext]'
+    },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+   ]
+};
